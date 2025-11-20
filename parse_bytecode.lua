458bc19524e42b1fee8446909a13bd8c469ee1c8
nicejs-is-cool <nicejsalt@gmail.com>
2025-11-20 13:27:51 -0300

    the uhh 2

--- parse_bytecode.lua
+++ parse_bytecode.lua
@@ -1,11 +1,22 @@
-local function getOpcode(i)
-    return (((((i)>>0) & ((~((~0)<<(7)))<<(0)))))
+local lcc = { -- from luac.c
+    POS_OP = 0,
+    SIZE_OP = 7,
+}
+function lcc.MASK1(n, p)
+    return ((~((~0)<<(n)))<<(p))
+end
+function lcc.MASK0(n, p)
+    return (~lcc.MASK1(n,p))
+end
+
+function lcc.GET_OPCODE(i)
+    return ((i)>>lcc.POS_OP) & lcc.MASK1(lcc.SIZE_OP,0)
 end
 
 ---parse instruction
 ---@param instr integer
 local function parseInstruction(instr)
-    local opcode = getOpcode(instr)
+    local opcode = lcc.GET_OPCODE(instr)
     print("opcode", opcode)
 end
 
